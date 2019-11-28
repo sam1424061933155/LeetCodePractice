098cfa54580aa464d6b203ce492bcacc45bfa679
sam1424061933155 <sam1424061933155@gmail.com>
2019-11-28 21:46:38 +0800

    Practice

---
+++ Sqrt.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+# include <vector>
+# include <string>
+# include <stack>
+# include <map>
+
+using namespace std ;
+
+
+int mySqrt(int x) {
+    int ans = floor(sqrt(x));
+    return ans;
+}
