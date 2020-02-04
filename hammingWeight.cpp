2656604b4421c07bff3a41917cc10d240dd6df5f
sam1424061933155 <sam1424061933155@gmail.com>
2020-02-04 20:38:14 +0800

    #191

---
+++ hammingWeight.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        
+        while(n>0){
+            int temp = n%2;
+            if(temp == 1) count++;
+            n = n/2;
+        }
+        
+        return count;
+    }
+};
